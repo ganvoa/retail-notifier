ddbf63bcca7c0923ee764e096371290746964647
gamboa <ngamboa@insacom.cl>
2022-03-05 18:42:50 -0300

    wip: docker env

--- Dockerfile
+++ Dockerfile
@@ -3,9 +3,10 @@ WORKDIR /usr/app
 COPY yarn.lock .
 COPY package.json .
 RUN npx yarn install \
-    && npm install -g typescript 
+    && npm install -g typescript pm2
 COPY src src
 COPY app app
 COPY tsconfig.json .
+COPY processes.json .
 RUN tsc
-ENTRYPOINT ["node"]
+CMD ["pm2", "start", "--no-daemon", "processes.json"]
--- src/Domain/DepartmentFalabella.ts
+++ src/Domain/DepartmentFalabella.ts
@@ -1,10 +1,9 @@
 import { Department } from "./Department";
 
 export const DepartmentFalabella: { [key: string]: Department } = {
-    'cat9360001/Mascotas': Department.Mascotas,
     'cat16510006/Electrohogar': Department.Electro,
     'cat7090034/Tecnologia': Department.Tecnologia,
-    'cat12440001/Zapatos-Mujer': Department.Mujer,
-    'cat7660002/Belleza--higiene-y-salud': Department.Belleza,
-    'cat1008/Muebles': Department.Muebles,
+    // 'cat12440001/Zapatos-Mujer': Department.Mujer,
+    // 'cat7660002/Belleza--higiene-y-salud': Department.Belleza,
+    // 'cat1008/Muebles': Department.Muebles,
 }
