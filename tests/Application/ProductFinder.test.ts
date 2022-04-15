a1cfabf9c5f03169f28eeb2f7b9373b91da0333c
gamboa <ngamboa@insacom.cl>
2022-04-15 10:17:33 -0400

    wip: testing

--- tests/Application/ProductFinder.test.ts
+++ tests/Application/ProductFinder.test.ts
@@ -1,5 +1,5 @@
 import { DirectBroker } from "../../src/Domain/Broker";
-import { mock, when, instance, anything } from 'ts-mockito';
+import { mock, when, instance, anything, verify, anyNumber, anyString } from 'ts-mockito';
 import { HttpClient } from "../../src/Domain/HttpClient";
 import { Department } from "../../src/Domain/Department";
 import { AbcdinPageFetcher } from "../../src/Infrastructure/Retail/AbcdinPageFetcher";
@@ -7,30 +7,60 @@ import { AbcdinProductParser } from "../../src/Infrastructure/Retail/AbcdinProdu
 import { Paginator } from "../../src/Domain/Paginator";
 import { ProductFinder } from "../../src/Application/ProductFinder";
 import { Abcdin } from "../../src/Infrastructure/Retail/Abcdin";
+import * as fs from 'fs';
+import { Event } from "../../src/Domain/Event";
+import { PageFetcher } from "../../src/Domain/PageFetcher";
+import { ProductParser } from "../../src/Domain/ProductParser";
+import { Product } from "../../src/Domain/Product";
+import { Retail } from "../../src/Domain/Retail";
+
+beforeEach(() => {
+    jest.spyOn(console, 'log').mockImplementation(() => { });
+});
 
 describe('Product Finder', () => {
-    it('should find abcdin products', async () => {
+    it('should find products', async () => {
         let mockedBroker: DirectBroker = mock<DirectBroker>();
         const broker: DirectBroker = instance(mockedBroker);
-        let mockedHttpClient: HttpClient = mock<HttpClient>();
-        when(mockedHttpClient.get(anything())).thenReturn(Promise.resolve({
-            body: `toolbar-number">30</`,
-            statusCode: 200
-        }));
-        const httpClient: HttpClient = instance(mockedHttpClient);
-        const department = {
-            iterable: true,
-            department: Department.Muebles,
-            minDiscount: 60,
-            slug: 'test'
+
+        let mockedPageFetcher: PageFetcher = mock<PageFetcher>();
+        const pageFetcher: PageFetcher = instance(mockedPageFetcher);
+
+        const products: Product[] = [];
+        const product: Product = {
+            productId: '1234567',
+            brand: 'generic',
+            currentPrice: 10000,
+            exclusivePrice: 5000,
+            normalPrice: 10000,
+            minPrice: 5000,
+            department: Department.Tecnologia,
+            discountPercentage: 50,
+            name: 'Generic Product',
+            retailId: Retail.Ripley,
+            timestamp: Date.now(),
+            valid: true,
+            imageUrl: `https://home.ripley.cl/store/Attachment/WOP/D328/2000374180238/2000374180238_2.jpg`,
+            productUrl: `https://simple.ripley.cl/set-body-secret-mandala-2000374180238p`,
+            shouldNotify: false,
+            shouldStore: true
         };
-        const pageFetcher = new AbcdinPageFetcher(department, httpClient);
-        const totalCount = await pageFetcher.getTotalCount();
-        const productParser = new AbcdinProductParser(department);
-        const paginator = new Paginator(Abcdin.ITEMS_PER_PAGE, totalCount);
+
+        for (let index = 0; index < 16; index++) {
+            let pr = product;
+            pr.productId = pr.productId + index;
+            products.push(pr);
+        }
+
+        let mockedProductParser: ProductParser = mock<ProductParser>();
+        when(mockedProductParser.getAll(anyString())).thenReturn(products);
+        const productParser: ProductParser = instance(mockedProductParser);
+        const paginator = new Paginator(16, 160);
         const app = new ProductFinder(pageFetcher, productParser, paginator, broker);
         await app.start();
         await broker.close();
-        expect(totalCount).toBe(30);
+
+        verify(mockedPageFetcher.getPage(16, anyNumber(), anyNumber())).times(16);
+        verify(mockedBroker.publish(Event.ProductFound, anything())).times(160);
     })
 })
